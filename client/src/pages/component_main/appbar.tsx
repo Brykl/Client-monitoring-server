fa669f10af6855f40df03e283b5c379747b0a7b5
Brykl <rokirok430@gmail.com>
2024-12-01 15:45:00 +0500

    set up bar's theme

--- client/src/pages/component_main/appbar.tsx
+++ client/src/pages/component_main/appbar.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,11 +5,11 @@ import Typography from '@mui/material/Typography';
 
 export default function DenseAppBar() {
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static" sx={{bgcolor: '#5449a3'}}>
+    <Box  sx={{ flexGrow: 1}}>
+      <AppBar position="static" sx={{bgcolor: '#66754c'}}>
         <Toolbar variant="dense" sx={{ justifyContent: 'center' }}>
-          <Typography variant="h5" color="inherit" component="div" >
-            Monitor your server
+          <Typography variant="h5" color="#cccccc" component="div" >
+            Server Monitoring
           </Typography>
         </Toolbar>
       </AppBar>
